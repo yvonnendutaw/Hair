7d5f52190ea851b031e491fb3b307a10bb773079
yvonnendutaw <yvonita7@gmail.com>
2016-05-30 10:11:45 +0300

    added files

--- app/src/main/java/com/example/yvonne/hair/Baafro.java
+++ app/src/main/java/com/example/yvonne/hair/Baafro.java
@@ -3,7 +3,7 @@ package com.example.yvonne.hair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class BAAfro extends AppCompatActivity {
+public class Baafro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
